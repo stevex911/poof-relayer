ff1761b89eda0d0517bb8eec86e3c053647d4c7d
Brian Li <brnli7@gmail.com>
2021-09-19 14:44:09 -0700

    Add fetcher for cUSD price

--- src/priceWatcher.js
+++ src/priceWatcher.js
@@ -1,39 +1,26 @@
 const Redis = require('ioredis')
-const {redisUrl, offchainOracleAddress, oracleRpcUrl} = require('./config')
-const {getArgsForOracle, setSafeInterval} = require('./utils')
+const { redisUrl, oracleRpcUrl } = require('./config')
+const { setSafeInterval } = require('./utils')
 const redis = new Redis(redisUrl)
+const { newKitFromWeb3 } = require('@celo/contractkit')
+const { fromWei, toWei } = require('web3-utils')
 const Web3 = require('web3')
 const web3 = new Web3(
   new Web3.providers.HttpProvider(oracleRpcUrl, {
     timeout: 200000, // ms
   }),
 )
-
-const offchainOracleABI = require('../abis/OffchainOracle.abi.json')
-
-const offchainOracle = new web3.eth.Contract(offchainOracleABI, offchainOracleAddress)
-const {tokenAddresses, oneUintAmount, currencyLookup} = getArgsForOracle()
-
-const {toBN} = require('web3-utils')
+const kit = newKitFromWeb3(web3)
 
 async function main() {
   try {
     // TODO: poof price, rcelo price
-    const celoPrices = {'celo': 1.0, 'poof': 1.0, 'rcelo': 0.000015384615, 'cusd': 0.2}
-    // for (let i = 0; i < tokenAddresses.length; i++) {
-    //   try {
-    //     const price = await offchainOracle.methods
-    //       .getRate(tokenAddresses[i], '0x0000000000000000000000000000000000000000')
-    //       .call()
-    //     const numerator = toBN(oneUintAmount[i])
-    //     const denominator = toBN(10).pow(toBN(18)) // eth decimals
-    //     const priceFormatted = toBN(price).mul(numerator).div(denominator)
 
-    //     celoPrices[currencyLookup[tokenAddresses[i]]] = priceFormatted.toString()
-    //   } catch (e) {
-    //     console.error('cant get price of ', tokenAddresses[i])
-    //   }
-    // }
+    // Get cUSD price in CELO
+    const exchange = await kit.contracts.getExchange()
+    const celoPriceCusd = Number(fromWei((await exchange.quoteGoldSell(toWei('1'))).toString()))
+
+    const celoPrices = { celo: 1.0, poof: 1.0, rcelo: 0.000015384615, cusd: 1 / celoPriceCusd }
 
     await redis.hmset('prices', celoPrices)
     console.log('Wrote following prices to redis', celoPrices)
